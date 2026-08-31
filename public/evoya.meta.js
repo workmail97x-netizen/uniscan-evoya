b448dd00968c4b6c597dae512c2f62d01c186fe0
workmail97x-netizen <workmail97x@gmail.com>
2026-08-31 10:04:01 +0300

    Bump UniScan Evoya metadata to 1.3.4

--- public/evoya.meta.js
+++ public/evoya.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         UniScan Evoya
 // @namespace    uniscan.evoya
-// @version      1.3.3
-// @description  UniScan للغربلة: يعمل فقط بصفحة الإدخال لتخفيف تسجيل الدخول على iPhone.
+// @version      1.3.4
+// @description  UniScan للغربلة: يمنع التركيز التلقائي بعد المسح بدون تعطيل البحث اليدوي والاقتراحات.
 // @match        https://iraq-central-moh-nbs.evoya.revvitycloud.com/home/management/remote-demographic-entry*
 // @run-at       document-idle
 // @noframes
