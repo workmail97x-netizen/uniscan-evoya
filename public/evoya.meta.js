9c5f884b098e8fb90c23cbe83c302594bcbe127d
workmail97x-netizen <workmail97x@gmail.com>
2026-08-31 10:49:10 +0300

    v1.3.8: update userscript metadata

--- public/evoya.meta.js
+++ public/evoya.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         UniScan Evoya
 // @namespace    uniscan.evoya
-// @version      1.3.7
-// @description  UniScan للغربلة: تفعيل تلقائي داخل Evoya SPA + مسح الباركود بدون إثقال تسجيل الدخول.
+// @version      1.3.8
+// @description  UniScan للغربلة: تفعيل Evoya SPA + ماسح باركود ملء الشاشة على الهاتف.
 // @match        https://iraq-central-moh-nbs.evoya.revvitycloud.com/*
 // @run-at       document-idle
 // @noframes
