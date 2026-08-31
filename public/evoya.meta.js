8b8ad04f597cc5385bcf5d6e1593e51ea41f40fb
workmail97x-netizen <workmail97x@gmail.com>
2026-08-31 10:10:50 +0300

    Bump UniScan Evoya metadata to v1.3.5

--- public/evoya.meta.js
+++ public/evoya.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         UniScan Evoya
 // @namespace    uniscan.evoya
-// @version      1.3.4
-// @description  UniScan للغربلة: يمنع التركيز التلقائي بعد المسح بدون تعطيل البحث اليدوي والاقتراحات.
+// @version      1.3.5
+// @description  UniScan للغربلة: إصلاح عرض اقتراحات Evoya على الهاتف بعد مسح الباركود.
 // @match        https://iraq-central-moh-nbs.evoya.revvitycloud.com/home/management/remote-demographic-entry*
 // @run-at       document-idle
 // @noframes
